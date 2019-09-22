15ce06154e69c61254ff36f63df7af75f39056c1
Alexander Sandoval <alexander.sf0828@gmail.com>
2019-09-22 10:18:23 -0500

    Implement middleware (and a custom)

--- app.js
+++ app.js
@@ -3,8 +3,18 @@ import express from 'express';
 // bring in routes
 import { getPosts } from './routes/post.js';
 
+// middleware
+import morgan from 'morgan';
+const customMiddleware = (req, res, next) => {
+  console.log('middleware applied');
+  next();
+};
+
 const app = express();
 const port = 8080;
+
+app.use(morgan('dev'));
+app.use(customMiddleware);
 
 app.get('/', getPosts);
 
