6cd637275cba028e29791122afe02902cc5ba430
Alexander Sandoval <alexander.sf0828@gmail.com>
2019-09-22 13:09:08 -0500

    Connect to a MongoDB (local and remote)

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 // bring in routes
 import router from './routes/post.js';
@@ -7,7 +9,23 @@ import router from './routes/post.js';
 import morgan from 'morgan';
 
 const app = express();
-const port = 8080;
+dotenv.config();
+const port = process.env.PORT || 8080;
+
+// DB
+mongoose
+  .connect(process.env.MONGO_URI, {
+    // prevent warnings
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  })
+  .then(() => {
+    console.log('DB connected');
+  });
+
+mongoose.connection.on('error', err => {
+  console.log(`DB conecction error: ${err.message}`);
+});
 
 app.use(morgan('dev'));
 app.use('/', router);
