6161bc6676a46c225014d5ec826eef8b46611f1b
Alexander Sandoval <alexander.sf0828@gmail.com>
2019-09-22 10:09:43 -0500

    Implement esm modules and create routes file for posts

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-const express = require('express');
+import express from 'express';
+
+// bring in routes
+import { getPosts } from './routes/post.js';
+
 const app = express();
 const port = 8080;
+
+app.get('/', getPosts);
 
 app.listen(port, () => console.log(`Node.js API listening in port ${port}`));
